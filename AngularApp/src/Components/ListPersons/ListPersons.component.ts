61e00cf162bddcdeb840a5d4f95bd614deb96139
Jas <jas3542@hotmail.com>
2018-03-15 14:01:48 +0000

    filter on persons list working.

--- AngularApp/src/Components/ListPersons/ListPersons.component.ts
+++ AngularApp/src/Components/ListPersons/ListPersons.component.ts
@@ -14,19 +14,46 @@ export class ListPersonsComponent implements OnInit {
   
   personArray: Person[];
 
+  filteredPerson: Person[];
+  _listFilter: string;
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredPerson = this.listFilter ? this.performFilter(this.listFilter) : this.personArray;
+  }
+
   constructor(private _httpService: Http, private _personListService: PersonListService, private _router: Router) {
 
   }
 
   ngOnInit() {
     console.log("Init - ListPersons");
+    //this._listFilter = 'cart';
+
     this.getPersonsList();
+    this.getFilteredPersonsList();
+    
   }
 
   getPersonsList(): void {
     this._personListService.getPersonsList().subscribe(values => {
       this.personArray = values;
     });
+  }
+
+  getFilteredPersonsList(): void {
+    this._personListService.getPersonsList().subscribe(values => {
+      this.filteredPerson = values;
+    });
+  }
+
+  performFilter(filterBy: string): Person[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.personArray.filter((person) => 
+      person.name.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
   
 }
